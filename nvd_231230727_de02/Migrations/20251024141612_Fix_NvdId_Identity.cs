630654278f15c0fa0ae808e1594426e238938063
dung231230727@lms.utc.edu.vn <dung231230727@lms.utc.edu.vn>
2025-10-24 21:20:19 +0700

    fix NvdId tự tăng trên vs, chỉnh fontend

---
+++ nvd_231230727_de02/Migrations/20251024141612_Fix_NvdId_Identity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nvd_231230727_de02.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_NvdId_Identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NvdCatalog",
+                columns: table => new
+                {
+                    nvdId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nvdCateName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    nvdCatePrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    nvdCateQty = table.Column<int>(type: "int", nullable: false),
+                    nvdPicture = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    nvdCateActive = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__NvdCatal__CA45E3779A41E60A", x => x.nvdId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NvdCatalog");
+        }
+    }
+}
